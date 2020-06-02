3060d1aa38bd1733d3e3b6881103d49c1ad23611
OT <imot@protonmail.com>
2020-06-02 18:38:07 +0400

    Add files via upload

--- upp.sh
+++ upp.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
+
+alias ~=$HOME
 bold=$(tput bold)
 normal=$(tput sgr0)
-banner="# ./07.dat\n\t\t ${bold}_\n\t0${normal}${bold}7${normal}\t[|] ${bold}0${normal}ffensive ${bold}7${normal}ester\n\n"
+banner="# ./07.dat\n\t\t ${bold}_\n\t0${normal}${bold}7${normal}\t[|] ${bold}0${normal}ffensive ${bold}7${normal}ester\n"
 clear
-echo -en "#\r"; sleep 0.1
-echo -en "# .\r"; sleep 0.1
-echo -en "# ./\r"; sleep 0.1
-echo -en "# ./0\r"; sleep 0.1
-echo -en "# ./07\r"; sleep 0.1
-echo -en "# ./07.\r"; sleep 0.2
-echo -en "# ./07.d\r"; sleep 0.2
-echo -en "# ./07.da\r"; sleep 0.2
-echo -en "# ./07.dat\r"; sleep 0.2
+text="# ./07.dat"
+for((i=0; i<=${#text}; i++)); do
+		printf "${text::$i}\r";
+		if [ $i -lt $(($i/2)) ]; then
+			sleep 0.2
+		else
+			sleep 0.1
+		fi
+done
 echo -e ${banner} && echo -e "
-
                    ▄
                    █▀
 user2014
@@ -28,38 +29,24 @@ user2014
 
 
 "
-function cleartemp {
-	if [[ -f /tmp/upp.dat ]]; then
-		rm -rf /tmp/upp.dat
-	fi
-}
-if ! [[ $1 ]]; then
-	echo -e "\t*select file location: [/root/Desktop/pass.list]"
-	read passlist
-else
+if [ $# -gt 0 ]; then
 	passlist=$1
 fi
-if [[ $passlist == "" ]]; then
-	echo -e "\tsorry, but select a file location for save passlist is too important!\n"
-	exit 1
-fi
-if [ -d $passlist ]; then
-	echo -e "\terror: this is directory name!\n"
-	exit 1
-fi
-if [[ ${passlist:00:01} == "~" ]]; then
-	passlist=$HOME${passlist:1}
-fi
-setfile=$(echo $passlist | sed 's/\// /g') && setfilearray=($setfile) && file=${setfilearray[-1]}
-setdirectory=$(echo $passlist |  sed 's/.'$file'//') && directory=$setdirectory
+while true; do
+	if [[ $passlist == "" ]]; then
+		echo -e "\tERROR: input file directory\n"
+	elif [ -d $passlist ]; then
+		echo -e "\tERROR: this is directory name\n"
+	elif ! touch "$passlist" &> /dev/null; then
+		echo -e "\tERROR: can't create (can be a permission issue. try use \"sudo\")\n"
+	fi
+	if [ ${#passlist} -gt 0 ] && [ -f $passlist ]; then
+		break
+	fi
+	echo -e "\t*select file location: [/root/Desktop/pass.list]"
+	read passlist
+done
 buffersizemax=50000
-if [ ! -d $directory ]; then
-	echo -e "\terror: can't create file on this directory!\n"
-	exit 1
-fi
-if [ ! -f $passlist ]; then
-	touch $passlist || echo "ERROR: can't create "$passlist" (can be a permission issue. try use \"sudo\")"
-fi
 echo -e "\ttype words: [facebook password qwerty 1996]"
 read words
 while [[ $words == "" ]]; do
@@ -67,15 +54,20 @@ while [[ $words == "" ]]; do
 	read words
 done
 wordarray=($words)
-	for (( a = 0; a < ${#wordarray[@]}; a++ )); do
-		for (( b = 0; b < ${#wordarray[@]}; b++ )); do
-			if [[ $a != $b ]]; then
-				addWord+="${wordarray[a]}${wordarray[b]} "
-			fi
-		done
-	done
-wordarray+=($addWord)
-sed 's/ /\n/g' <<< ${wordarray[@]} >> $passlist
+buffersize=0
+for ((i=0; i<${#wordarray[@]}; i++)); do
+		currentword=${wordarray[$i]}
+		if [ $buffersize -eq 0 ]; then
+			buffer+="$currentword"
+		else
+			buffer+="\n$currentword"
+		fi
+		if [ $buffersize -eq $buffersizemax ]; then
+			echo -e $buffer >> $passlist
+			buffer=""; buffersize=0
+		fi
+	let buffersize++
+done; echo -e $buffer >> $passlist; buffer=""
 while true; do
 	opinion=""
 	how=""
@@ -89,7 +81,7 @@ while true; do
 	fi
 	opinionarray=($opinion)
 	opiniontruearray=(0 1 11 12 2 3 31 32 33 34 35 4 41 42 43 44 45 5 51 52 53 54 55 6)
-	optionnotsinglearray=(1 3 4 5)
+	optionnotsinglearray=(1 2 3 4 5)
 	if [[ ${#opinionarray[@]} < 1 ]]; then
 		for((i = 0; i < ${#opinionarray[@]}; i++)); do
 			for((j=0; j < ${#optionnotsinglearray[@]}; j++)); do
@@ -176,10 +168,9 @@ while true; do
 					echo -e "\tleet mode: normal mode"
 				fi
 				(
-				leet=$(sed -e 's/a/4/g' -e 's/A/4/g' -e 's/e/3/g' -e 's/E/3/g' -e 's/g/9/g' -e 's/G/9/g' \
+				sed -e 's/a/4/g' -e 's/A/4/g' -e 's/e/3/g' -e 's/E/3/g' -e 's/g/9/g' -e 's/G/9/g' \
 						-e 's/i/1/g' -e 's/I/1/g' -e 's/o/0/g' -e 's/O/0/g' -e 's/s/5/g' -e 's/S/5/g' \
-						-e 's/t/7/g' -e 's/T/7/g' -e 's/z/2/g' -e 's/Z/2/g' <<< ${wordarray[@]})
-				tr " " "\n" <<< $leet >> $passlist
+						-e 's/t/7/g' -e 's/T/7/g' -e 's/z/2/g' -e 's/Z/2/g' <<< ${wordarray[@]} |tr " " "\n" >> $passlist
 				) | echo -en "\n\tloading...\r" && echo -en "\tdone!                \r\n\n"
 			;;
 			2)
@@ -187,7 +178,7 @@ while true; do
 				echo -e "\tleet mode: super mode"
 			fi
 				(
-				superLeet=$(sed -e 's/a/\/-\\/g' -e 's/A/\/-\\/g' -e 's/b/\|3/g' -e 's/B/\|3/g' \
+				sed -e 's/a/\/-\\/g' -e 's/A/\/-\\/g' -e 's/b/\|3/g' -e 's/B/\|3/g' \
 						-e 's/c/\(/g' -e 's/C/\(/g' -e 's/d/\|\)/g' -e 's/D/\|\)/g' \
 						-e 's/e/3/g' -e 's/E/3/g' -e 's/g/9/g' -e 's/G/9/g' \
 						-e 's/h/\|-\|/g' -e 's/H/\|-\|/g' -e 's/i/1/g' -e 's/I/1/g' \
@@ -196,9 +187,7 @@ while true; do
 						-e 's/r/\|2/g' -e 's/R/\|2/g' -e 's/o/0/g' -e 's/O/0/g' \
 						-e 's/s/5/g' -e 's/S/5/g' -e 's/t/7/g' -e 's/T/7/g' -e 's/u/\|_\|/g' \
 						-e 's/u/\|_\|/g' -e 's/v/\\\//g' -e 's/V/\\\//g' -e 's/w/vv/g' -e 's/W/vv/g' \
-						-e 's/x/\>\</g' -e 's/X/\>\</g' -e 's/z/2/g' -e 's/Z/2/g' <<< ${wordarray[@]})
-
-				tr " " "\n" <<< $superLeet >> $passlist
+						-e 's/x/\>\</g' -e 's/X/\>\</g' -e 's/z/2/g' -e 's/Z/2/g' <<< ${wordarray[@]} |tr " " "\n" >> $passlist
 				) | echo -en "\n\tloading...\r" && echo -en "\tdone!                \r\n\n"
 			;;
 			*)
@@ -214,7 +203,7 @@ while true; do
 			numbword=0
 			for((i=0; i<${#wordarray[@]}; i++)); do
 				currentword=${wordarray[$i]}
-				eval echo $(echo $currentword | sed 's/./{\U&,\L&}/g')|tr " " "\n" >> $passlist
+				tr " " "\n" <<< $(eval echo $(sed 's/./{\U&,\L&}/g' <<< $currentword)) >> $passlist
 			done
 			) | echo -en "\n\tloading...\r" && echo -en "\tdone!                \r\n\n"
 		;;
@@ -258,14 +247,18 @@ while true; do
 								zero+="0"
 							done 
 							numbers=$zero$j
-							buffer+=" $numbers$currentword"
+							if [ $buffersize -eq 0 ]; then
+								buffer+="$numbers$currentword"
+							else
+								buffer+="\n$numbers$currentword"
+							fi
 							if [ $buffersize -eq $buffersizemax ]; then
-								tr " " "\n" <<< $buffer >> $passlist
+								echo -e $buffer >> $passlist
 								buffer=""; buffersize=0
 							fi
 							let buffersize++
 						done
-					done; tr " " "\n" <<< $buffer >> $passlist; buffer=""
+					done; echo -e $buffer >> $passlist; buffer=""
 					) | echo -en "\n\tloading...\r" && echo -en "\tdone!                \r\n\n"
 				;;
 				2)
@@ -285,14 +278,18 @@ while true; do
 								zero+="0"
 							done 
 							numbers=$zero$j
-							buffer+=" $currentword$numbers"
+							if [ $buffersize -eq 0 ]; then
+								buffer+="$currentword$numbers"
+							else
+								buffer+="\n$currentword$numbers"
+							fi
 							if [ $buffersize -eq $buffersizemax ]; then
-								tr " " "\n" <<< $buffer >> $passlist
+								echo -e $buffer >> $passlist
 								buffer=""; buffersize=0
 							fi
 							let buffersize++
 						done
-					done; tr " " "\n" <<< $buffer >> $passlist; buffer=""
+					done; echo -e $buffer >> $passlist; buffer=""
 				;;
 				3)
 					if [[ ${#opinionarray[@]} == 1 ]]; then
@@ -312,15 +309,20 @@ while true; do
 								zero+="0"
 							done 
 							numbers=$zero$j
-							buffer+=" $numbers$currentword"
-							buffer+=" $currentword$numbers"
+							if [ $buffersize -eq 0 ]; then
+								buffer+="$numbers$currentword"
+								buffer+="\n$currentword$numbers"
+							else
+								buffer+="\n$numbers$currentword"
+								buffer+="\n$currentword$numbers"
+							fi
 							if [ $buffersize -eq $buffersizemax ]; then
-								tr " " "\n" <<< $buffer >> $passlist
+								echo -e $buffer >> $passlist
 								buffer=""; buffersize=0
 							fi
 							let buffersize++
 						done
-					done; tr " " "\n" <<< $buffer >> $passlist; buffer=""
+					done; echo -e $buffer >> $passlist; buffer=""
 					) | echo -en "\n\tloading...\r" && echo -en "\tdone!                \r\n\n"
 				;;
 				4)
@@ -341,14 +343,18 @@ while true; do
 								zero+="0"
 							done 
 							numbers=$zero$j
-							buffer+=" $numbers$currentword$numbers"
+							if [ $buffersize -eq 0 ]; then
+								buffer+="$numbers$currentword$numbers"
+							else
+								buffer+="\n$numbers$currentword$numbers"
+							fi
 							if [ $buffersize -eq $buffersizemax ]; then
-								tr " " "\n" <<< $buffer >> $passlist
+								echo -e $buffer >> $passlist
 								buffer=""; buffersize=0
 							fi
 							let buffersize++
 						done
-					done; tr " " "\n" <<< $buffer >> $passlist; buffer=""
+					done; echo -e $buffer >> $passlist; buffer=""
 					) | echo -en "\n\tloading...\r" && echo -en "\tdone!                \r\n\n"
 				;;
 				5)
@@ -365,20 +371,26 @@ while true; do
 						currentword=${wordarray[$i]}
 						for((j=$minimum; j<=$maximum; j++)); do
 							zero="";
-							for((z=0; z<${#minimum}-${#j}; z++)) do
+							for((z=0; z<${#minimum}-${#j}; z++)); do
 								zero+="0"
-							done 
+							done
 							numbers=$zero$j
-							buffer+=" $numbers$currentword"
-							buffer+=" $numbers$currentword$numbers"
-							buffer+=" $currentword$numbers"
+							if [ $buffersize -eq 0 ]; then
+								buffer+="$numbers$currentword"
+								buffer+="\n$numbers$currentword$numbers"
+								buffer+="\n$currentword$numbers"
+							else
+								buffer+="\n$numbers$currentword"
+								buffer+="\n$numbers$currentword$numbers"
+								buffer+="\n$currentword$numbers"
+							fi
 							if [ $buffersize -eq $buffersizemax ]; then
-								tr " " "\n" <<< $buffer >> $passlist
+								echo -e $buffer >> $passlist
 								buffer=""; buffersize=0
 							fi
 							let buffersize++
 						done
-					done; tr " " "\n" <<< $buffer >> $passlist; buffer=""
+					done; echo -e $buffer >> $passlist; buffer=""
 					) | echo -en "\n\tloading...\r" && echo -en "\tdone!                \r\n\n"
 				;;
 				*)
@@ -421,13 +433,23 @@ while true; do
 				(
 				numbword=0
 				numbspecial=0
+				buffersize=0
 				for((i=0; i<${#wordarray[@]}; i++)); do
 					currentword=${wordarray[$i]}
 					for((j=0; j<${#special[@]}; j++)); do
 						specials=${special[$j]}
-						tr " " "\n" <<< "$specials$currentword $j" >> $passlist
+						if [ $buffersize -eq 0 ]; then
+							buffer+="$specials$currentword"
+						else
+							buffer+="\n$specials$currentword"
+						fi
+						if [ $buffersize -eq $buffersizemax ]; then
+							echo -e $buffer >> $passlist
+							buffer=""; buffersize=0
+						fi
+						let buffersize++
 					done
-				done
+				done; echo -e $buffer >> $passlist; buffer=""
 				) | echo -en "\n\tloading...\r" && echo -en "\tdone!                \r\n\n"
 			;;
 			2)
@@ -439,13 +461,23 @@ while true; do
 				(
 				numbword=0
 				numbspecial=0
+				buffersize=0
 				for((i=0; i<${#wordarray[@]}; i++)); do
 					currentword=${wordarray[$i]}
 					for((j=0; j<${#special[@]}; j++)); do
 						specials=${special[$j]}
-						tr " " "\n" <<< "$currentword$specials" >> $passlist
+						if [ $buffersize -eq 0 ]; then
+							buffer+="$currentword$specials"
+						else
+							buffer+="\n$currentword$specials"
+						fi
+						if [ $buffersize -eq $buffersizemax ]; then
+							echo -e $buffer >> $passlist
+							buffer=""; buffersize=0
+						fi
+						let buffersize++
 					done
-				done
+				done; echo -e $buffer >> $passlist; buffer=""
 				) | echo -en "\n\tloading...\r" && echo -en "\tdone!                \r\n\n"
 			;;
 			3)
@@ -457,14 +489,25 @@ while true; do
 				(
 				numbword=0
 				numbspecial=0
+				buffersize=0
 				for((i=0; i<${#wordarray[@]}; i++)); do
 					currentword=${wordarray[$i]}
 					for((j=0; j<${#special[@]}; j++)); do
 						specials=${special[$j]}
-						tr " " "\n" <<< "$specials$currentword" >> $passlist
-						tr " " "\n" <<< "$currentword$specials" >> $passlist
+						if [ $buffersize -eq 0 ]; then
+							buffer+="$specials$currentword"
+							buffer+="\n$currentword$specials"
+						else
+							buffer+="\n$specials$currentword"
+							buffer+="\n$currentword$specials"
+						fi
+						if [ $buffersize -eq $buffersizemax ]; then
+							echo -e $buffer >> $passlist
+							buffer=""; buffersize=0
+						fi
+						let buffersize++
 					done
-				done
+				done; echo -e $buffer >> $passlist; buffer=""
 				) | echo -en "\n\tloading...\r" && echo -en "\tdone!                \r\n\n"
 			;;
 			4)
@@ -476,13 +519,23 @@ while true; do
 				(
 				numbword=0
 				numbspecial=0
+				buffersize=0
 				for((i=0; i<${#wordarray[@]}; i++)); do
 					currentword=${wordarray[$i]}
 					for((j=0; j<${#special[@]}; j++)); do
 						specials=${special[$j]}
-						tr " " "\n" <<< "$specials$currentword$specials" >> $passlist
+						if [ $buffersize -eq 0 ]; then
+							buffer+="$specials$currentword$specials"
+						else
+							buffer+="\n$specials$currentword$specials"
+						fi
+						if [ $buffersize -eq $buffersizemax ]; then
+							echo -e $buffer >> $passlist
+							buffer=""; buffersize=0
+						fi
+						let buffersize++
 					done
-				done
+				done; echo -e $buffer >> $passlist; buffer=""
 				) | echo -en "\n\tloading...\r" && echo -en "\tdone!                \r\n\n"
 			;;
 			5)
@@ -494,15 +547,27 @@ while true; do
 				(
 				numbword=0
 				numbspecial=0
+				buffersize=0
 				for((i=0; i<${#wordarray[@]}; i++)); do
 					currentword=${wordarray[$i]}
 					for((j=0; j<${#special[@]}; j++)); do
 						specials=${special[$j]}
-						tr " " "\n" <<< "$specials$currentword" >> $passlist
-						tr " " "\n" <<< "$specials$currentword$specials" >> $passlist
-						tr " " "\n" <<< "$currentword$specials" >> $passlist
+						if [ $buffersize -eq 0 ]; then
+							buffer+="$specials$currentword"
+							buffer+="\n$specials$currentword$specials"
+							buffer+="\n$currentword$specials"
+						else
+							buffer+="\n$specials$currentword"
+							buffer+="\n$specials$currentword$specials"
+							buffer+="\n$currentword$specials"
+						fi
+						if [ $buffersize -eq $buffersizemax ]; then
+							echo -e $buffer >> $passlist
+							buffer=""; buffersize=0
+						fi
+						let buffersize++
 					done
-				done
+				done; echo -e $buffer >> $passlist; buffer=""
 				) | echo -en "\n\tloading...\r" && echo -en "\tdone!                \r\n\n"
 			;;
 			*)
@@ -537,94 +602,147 @@ while true; do
 			1)
 				if [[ ${#opinionarray[@]} == 1 ]]; then
 					echo -e "\tbeginning:"
-				elif [[ ${#opinionarray[@]} > 1 ]]; then
-					echo -e "\tadd some words for all: beginning"
+				elif [[ ${#opinionarray[@]} == 1 ]]; then
+					echo -e "\n\tsome:  beginning"
 				fi
 				(
 				numbword=0
 				numbsome=0
+				buffersize=0
 				for((i=0; i<${#wordarray[@]}; i++)); do
 					currentword=${wordarray[$i]}
 					for((j=0; j<${#some[@]}; j++)); do
 						somes=${some[$j]}
-						tr " " "\n" <<< "$somes$currentword" >> $passlist
+						if [ $buffersize -eq 0 ]; then
+							buffer+="$somes$currentword"
+						else
+							buffer+="\n$somes$currentword"
+						fi
+						if [ $buffersize -eq $buffersizemax ]; then
+							echo -e $buffer >> $passlist
+							buffer=""; buffersize=0
+						fi
+						let buffersize++
 					done
-				done
+				done; echo -e $buffer >> $passlist; buffer=""
 				) | echo -en "\n\tloading...\r" && echo -en "\tdone!                \r\n\n"
 			;;
 			2)
 				if [[ ${#opinionarray[@]} == 1 ]]; then
 					echo -e "\tend:"
 				elif [[ ${#opinionarray[@]} > 1 ]]; then
-					echo -e "\tadd some words for all:  end"
+					echo -e "\n\tsome: end"
 				fi
 				(
 				numbword=0
 				numbsome=0
+				buffersize=0
 				for((i=0; i<${#wordarray[@]}; i++)); do
 					currentword=${wordarray[$i]}
 					for((j=0; j<${#some[@]}; j++)); do
 						somes=${some[$j]}
-						tr " " "\n" <<< "$currentword$somes" >> $passlist
+						if [ $buffersize -eq 0 ]; then
+							buffer+="$currentword$somes"
+						else
+							buffer+="\n$currentword$somes"
+						fi
+						if [ $buffersize -eq $buffersizemax ]; then
+							echo -e $buffer >> $passlist
+							buffer=""; buffersize=0
+						fi
+						let buffersize++
 					done
-				done
+				done; echo -e $buffer >> $passlist; buffer=""
 				) | echo -en "\n\tloading...\r" && echo -en "\tdone!                \r\n\n"
 			;;
 			3)
 				if [[ ${#opinionarray[@]} == 1 ]]; then
 					echo -e "\tbeginning & end:"
 				elif [[ ${#opinionarray[@]} > 1 ]]; then
-					echo -e "\tadd some words for all: beginning & end"
+					echo -e "\n\tsome: beginning & end"
 				fi
 				(
 				numbword=0
 				numbsome=0
+				buffersize=0
 				for((i=0; i<${#wordarray[@]}; i++)); do
 					currentword=${wordarray[$i]}
 					for((j=0; j<${#some[@]}; j++)); do
 						somes=${some[$j]}
-						tr " " "\n" <<< "$somes$currentword" >> $passlist
-						tr " " "\n" <<< "$currentword$somes" >> $passlist
+						if [ $buffersize -eq 0 ]; then
+							buffer+="$somes$currentword"
+							buffer+="\n$currentword$somes"
+						else
+							buffer+="\n$somes$currentword"
+							buffer+="\n$currentword$somes"
+						fi
+						if [ $buffersize -eq $buffersizemax ]; then
+							echo -e $buffer >> $passlist
+							buffer=""; buffersize=0
+						fi
+						let buffersize++
 					done
-				done
+				done; echo -e $buffer >> $passlist; buffer=""
 				) | echo -en "\n\tloading...\r" && echo -en "\tdone!                \r\n\n"
 			;;
 			4)
 				if [[ ${#opinionarray[@]} == 1 ]]; then
 					echo -e "\tfor both side:"
 				elif [[ ${#opinionarray[@]} > 1 ]]; then
-					echo -e "\tadd some words for all:  for both side"
+					echo -e "\n\tsome: for both side"
 				fi
 				(
 				numbword=0
 				numbsome=0
+				buffersize=0
 				for((i=0; i<${#wordarray[@]}; i++)); do
 					currentword=${wordarray[$i]}
 					for((j=0; j<${#some[@]}; j++)); do
 						somes=${some[$j]}
-						tr " " "\n" <<< "$somes$currentword$somes" >> $passlist
+						if [ $buffersize -eq 0 ]; then
+							buffer+="$somes$currentword$somes"
+						else
+							buffer+="\n$somes$currentword$somes"
+						fi
+						if [ $buffersize -eq $buffersizemax ]; then
+							echo -e $buffer >> $passlist
+							buffer=""; buffersize=0
+						fi
+						let buffersize++
 					done
-				done
+				done; echo -e $buffer >> $passlist; buffer=""
 				) | echo -en "\n\tloading...\r" && echo -en "\tdone!                \r\n\n"
 			;;
 			5)
 				if [[ ${#opinionarray[@]} == 1 ]]; then
 					echo -e "\tall of them:"
 				elif [[ ${#opinionarray[@]} > 1 ]]; then
-					echo -e "\tadd some words for all: all of them"
+					echo -e "\n\tsome: all of them"
 				fi
 				(
 				numbword=0
 				numbsome=0
+				buffersize=0
 				for((i=0; i<${#wordarray[@]}; i++)); do
 					currentword=${wordarray[$i]}
 					for((j=0; j<${#some[@]}; j++)); do
 						somes=${some[$j]}
-						tr " " "\n" <<< "$somes$currentword" >> $passlist
-						tr " " "\n" <<< "$somes$currentword$somes" >> $passlist
-						tr " " "\n" <<< "$currentword$somes" >> $passlist
+						if [ $buffersize -eq 0 ]; then
+							buffer+="$somes$currentword"
+							buffer+="\n$somes$currentword$somes"
+							buffer+="\n$currentword$somes"
+						else
+							buffer+="\n$somes$currentword"
+							buffer+="\n$somes$currentword$somes"
+							buffer+="\n$currentword$somes"
+						fi
+						if [ $buffersize -eq $buffersizemax ]; then
+							echo -e $buffer >> $passlist
+							buffer=""; buffersize=0
+						fi
+						let buffersize++
 					done
-				done
+				done; echo -e $buffer >> $passlist; buffer=""
 				) | echo -en "\n\tloading...\r" && echo -en "\tdone!                \r\n\n"
 			;;
 			*)
@@ -638,8 +756,7 @@ while true; do
 				echo -e "\tremove equal words & sort:"
 			fi
 			(
-			echo $(sort $passlist | uniq -u) > $passlist
-			sed 's/ /\n/g' -i $passlist
+			sort $passlist | uniq -u > $passlist".bak"; mv $passlist".bak" $passlist
 			) | echo -en "\n\tloading...\r" && echo -en "\tdone!                \r\n\n"
 		;;
 		esac
